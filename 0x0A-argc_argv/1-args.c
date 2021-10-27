0f4c2fdf1ebace0f53f1b7ccdcd51bc85d500edb
haythem1a <haythem.abidi@icloud.com>
2021-10-27 11:40:43 -0700

    prints the name of argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main- prints it's own name
- * @argc : num of arguments
- * @argv : arr of arguments
+ * @argc : numbers of arguments
+ * @argv : arrays of arguments
  * Return: always 0
  */
 int main(__attribute__((unused))int argc, char *argv[])
