c244f9e94d95cf74767f5b02cff5b85f2bbc2795
haythem1a <haythem.abidi@icloud.com>
2022-12-13 13:23:52 +0100

    ABS

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute
+ * @r: int
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r > -1)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+	return (0);
+}
