42c2a90f924ef45755a7fdbc14e23a655fb76cfb
haythem1a <haythem.abidi@icloud.com>
2022-12-13 12:03:47 +0100

    print alphabet 10 times in lower case

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	int n;
+
+	while (i < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			putchar(n);
+		putchar('\n');
+		i++;
+	}
+}
