50217c34161719568581dc85b9e21464a830f9fd
haythem1a <haythem.abidi@icloud.com>
2022-12-13 11:20:33 +0100

    Print putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <unistd.h>
+/**
+ * main - print putchar
+ * Return: 0
+ */
+int main(void)
+
+{
+    printf("_putchar\n");
+    return (0);
+}
