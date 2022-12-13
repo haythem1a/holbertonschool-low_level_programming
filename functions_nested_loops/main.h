25078872d0bd6c1ca6c4cfadba46644ebae4923b
haythem1a <haythem.abidi@icloud.com>
2022-12-13 11:39:49 +0100

    MAINH

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <unistd.h>
-
+#include <ctype.h>
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,8 +16,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+void print_times_table(int);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
+int main(void);
 #endif
