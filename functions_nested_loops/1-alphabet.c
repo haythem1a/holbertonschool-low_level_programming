7deb5edd3228a5438898ab632e01ec7183a9f5e1
haythem1a <haythem.abidi@icloud.com>
2022-12-13 11:44:53 +0100

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ * Return: Alawys 0
+ */
+void print_alphabet(void)
+
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	putchar('\n');
+}
