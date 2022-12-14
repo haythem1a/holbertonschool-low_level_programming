4ad0e0a6a91529608d5dbfb7e4e3dafcd35cfed9
haythem1a <haythem.abidi@icloud.com>
2022-12-14 12:08:29 +0100

    addd2functions

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - two integer and show results
+ * @a: first int
+ * @b: second int
+ * Return: return somme
+ */
+
+int add(int a, int b)
+{
+	int h;
+
+	h = a + b;
+	return (h);
+}
