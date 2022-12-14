78ee64c6ffb432daf17ca6626d0f7a7e68dd5ac5
haythem1a <haythem.abidi@icloud.com>
2022-12-14 15:32:57 +0100

    TIMESTABLES

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+
+   	int num, i, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (i = 1; i <= n; i++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * i;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
